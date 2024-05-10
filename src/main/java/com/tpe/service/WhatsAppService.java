2221619a17bf3dc4f84e2007247ab034a2cdf22e
Ismayyl Atagarryyev <tkm.gala@gmail.com>
2024-05-10 20:10:00 +0300

    Spring MVC intro Day

--- src/main/java/com/tpe/service/WhatsAppService.java
+++ src/main/java/com/tpe/service/WhatsAppService.java
@@ -2,7 +2,7 @@ package com.tpe.service;
 
 import com.tpe.domain.Message;
 
-public class WhatsAppService {
+public class WhatsAppService implements MessageService {
 
     public void sendMessage(Message message){
         System.out.println("Your message has been sent via WhatsApp: "+message.getBody());
