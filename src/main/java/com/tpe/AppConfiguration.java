6b1e667978393c1b688c4f952bee99a6dca6f852
Ismayyl Atagarryyev <tkm.gala@gmail.com>
2024-05-14 18:37:36 +0300

    Spring MVC, Configuration

--- src/main/java/com/tpe/AppConfiguration.java
+++ src/main/java/com/tpe/AppConfiguration.java
@@ -3,12 +3,14 @@ package com.tpe;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 import java.util.Random;
 
 @Configuration //this class will be responsible for configuration
 @ComponentScan("com.tpe") //scans all classes in the same level packages
 //by default it will scan all classes in the same package, but we can also customize
+@PropertySource("classpath:application.properties")
 public class AppConfiguration {
 
 
--- src/main/java/com/tpe/service/MailService.java
+++ src/main/java/com/tpe/service/MailService.java
@@ -6,6 +6,7 @@ import com.tpe.repository.FileRepo;
 import com.tpe.repository.Repository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
@@ -17,6 +18,12 @@ import javax.annotation.PreDestroy;
 //@Scope (value = "singleton")  // by default it is singleton
 @Scope (value = "prototype")    // everytime the bean is requested, the new instance will be created
 public class MailService implements MessageService {
+
+    @Value("${email}")
+    private String email;
+
+    @Value("${phone}")
+    private String phone;
 
     @PostConstruct
     public void postConstractor () {
@@ -40,6 +47,11 @@ public class MailService implements MessageService {
     public void saveMessage(Message message) {
 //        Repository repo = new FileRepo();
 //        repo.save(message);
+    }
+
+    public void printContactsInfo () {
+        System.out.println("Your email: " + this.email);
+        System.out.println("Your phone Number: " + this.phone);
     }
 
 }
